1e1b7115dda088076f9d110d3d5381bd48009b4e
HEINRICK7 <heinrickcostta@gmail.com>
2025-03-03 13:43:06 -0300

    feate: ajustando socketio

--- src/pages/api/ip.ts
+++ src/pages/api/ip.ts
@@ -6,11 +6,11 @@ function getLocalIp(): string {
   for (const name of Object.keys(nets)) {
     for (const net of nets[name] || []) {
       if (net.family === "IPv4" && !net.internal) {
-        return net.address; // Retorna o IP real da máquina
+        return net.address;
       }
     }
   }
-  return "127.0.0.1"; // Fallback para localhost
+  return "127.0.0.1";
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
